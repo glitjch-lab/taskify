533371040824ac96e276ebabdbff6a5ed5b94e12
TJ Phan <t.jamesphan@gmail.com>
2022-04-06 10:55:18 -0700

    creaed condition to mark task as done when clicked on the done icon

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete, } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import './styles.css';
+import TodoList from './TodoList';
 
 type Props = {
   todo: Todo,
@@ -12,6 +13,14 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? {...todo, isDone: !todo.isDone} : todo
+      )
+    );
+  };
+
   return (
     <form className='todos__single'>
       <span className="todos__single--text">
@@ -20,7 +29,9 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
       <div>
         <span className="icon"><AiFillEdit /></span>
         <span className="icon"><AiFillDelete /></span>
-        <span className="icon"><MdDone /></span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
       </div>
 
     </form>
