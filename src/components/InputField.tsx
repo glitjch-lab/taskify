12ad94baafb8386e5bff046fd323e0b12bea59ad
TJ Phan <t.jamesphan@gmail.com>
2022-04-05 20:48:11 -0700

    fixed bug where submitting kept the screen dark

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css';
 
 interface Props {
@@ -8,11 +8,18 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='input' onSubmit={(e) => handleAdd(e)}>
+    <form 
+      className='input' 
+      onSubmit={(e) => {
+        handleAdd(e)
+        inputRef.current?.blur(); // --> prevents screen from staying dark after submission.
+    }}>
       <input 
         value={todo}
+        ref={inputRef}
         onChange={
           (e) => setTodo(e.target.value)
         }
