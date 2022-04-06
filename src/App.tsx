cdc74d7c790ff3b876115681b18c0d295083c88d
TJ Phan <t.jamesphan@gmail.com>
2022-04-05 18:24:38 -0700

    adding state to new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
 
 
 const App: React.FC = () => {
   const [ todo, setTodo] = useState<string>("");
+  const [todos, settodos] = useState<Todo[]>([]);
 
-  console.log('todo', todo);
+  const handleAdd = (e: React.FormEvent) => {
+     e.preventDefault();
+  }
   
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,13 @@ import './styles.css';
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
 
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
       <input 
         value={todo}
         onChange={
