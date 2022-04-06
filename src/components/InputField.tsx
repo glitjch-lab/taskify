0721e300142b62e8c3e1f9ce288b02134b44b224
TJ Phan <t.jamesphan@gmail.com>
2022-04-05 17:56:11 -0700

    created input responsiveness from parent component via props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+
   return (
     <form className='input'>
       <input 
+        value={todo}
+        onChange={
+          (e) => setTodo(e.target.value)
+        }
         type="input" 
         placeholder="Enter a task"
-        className='input__box' />
+        className='input__box' 
+
+      />
+
         <button className='input_submit' type='submit'>
           Go
         </button>
