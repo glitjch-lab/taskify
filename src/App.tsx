e6ff1a5c3b88a44294351b836a06081ff94493cc
TJ Phan <t.jamesphan@gmail.com>
2022-04-06 20:39:13 -0700

    added state to ready for dnd library

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Todo } from './model';
 const App: React.FC = () => {
   const [ todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [ completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
      e.preventDefault();
@@ -25,7 +26,12 @@ const App: React.FC = () => {
     <div className="App">
       <span className='heading'>Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+      <TodoList 
+        todos={todos} 
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}
+      />
     </div>
   );
 }
